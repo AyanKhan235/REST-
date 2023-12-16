604a90f0a4f6842ba4fad1de05c6878b85e66531
Ayan_khan_235 <babukhan640b@gmail.com>
2023-12-16 08:27:36 +0530

    rest api

--- index.js
+++ index.js
@@ -3,16 +3,46 @@ const app = express();
 const path=require("path");
 const port =8080;
 
+let posts=[
+    {
+        username:"AYAN",
+        content:" i am in 3rd year guys"
+
+    },
+    {
+        username:"Amaan",
+        content:"Amaan is a brother of developer"
+
+    }, {
+        username:"babu",
+        content:"this is my nick name "
+
+    },
+]
+
+
+
+
+
 app.use(express.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 
-app.get("/", (req,res)=>{
-    res.send("Server working well");
+app.get("/posts", (req,res)=>{
+    res.render("index.ejs", {posts});
 })
 
+app.get("/posts/new", (req,res)=>{
+    res.render("form.ejs");
+})
+app.post("/posts", (req,res)=>{
+    let {username, content}=req.body;
+    posts.push({username,content});
+    res.send("Working request properly"); 
+
+})
 
 
 

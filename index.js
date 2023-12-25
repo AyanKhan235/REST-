d610ad1ce5f3369147fa6f429e4271076c3f26b5
Ayan_khan_235 <babukhan640b@gmail.com>
2023-12-25 23:59:38 +0530

    crud operation is done in QUARA

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
 const path=require("path");
+const {v4 : uuidv4}=require('uuid');
 const port =8080;
+const methodOverride=require("method-override");
+app.use(methodOverride('_method'));
 
 let posts=[
-    {   id:"1a",
+    {   id:uuidv4(),
         username:"AYAN",
         content:" i am in 3rd year guys"
 
     },
-    {   id:"2b",
+    {   id:uuidv4(),
         username:"Amaan",
         content:"Amaan is a brother of developer"
 
     }, 
-    {   id:"3c", 
+    {   id:uuidv4(), 
         username:"babu",
         content:"this is my nick name "
 
@@ -44,9 +47,33 @@ app.get("/posts/:id", (req,res)=>{
     // console.log(post);
     res.render("show.ejs", {post});
 })
+
+app.patch("/posts/:id",(req,res)=>{ 
+    let{id}=req.params;
+    let newContent=req.body.content;
+    let post =posts.find((p)=> id===p.id);
+    post.content=newContent;
+    // console.log(post);
+    // res.send("patch Request Working");
+    res.redirect("/posts");
+
+});
+app.delete("/posts/:id", (req,res)=>{
+    let {id}=req.params;
+    // filter arrays ko return krega 
+     posts=posts.filter((p)=> id!==p.id);
+     res.redirect("/posts");
+})
+app.get("/posts/:id/edit",(req,res)=>{
+    let{id}=req.params;
+    let post=posts.find((p)=> id===p.id);
+    res.render("edit.ejs",{post});
+
+});
 app.post("/posts", (req,res)=>{
     let {username, content}=req.body;
-    posts.push({username,content});
+    let id=uuidv4();
+    posts.push({id, username,content});
     // res.send("Working request properly"); 
     res.redirect("/posts")
 
